a9265b3e5100b23444eab47ce8757a47658b2899
tezzyofficial01 <tezzyofficial01@gmail.com>
2025-07-24 18:49:13 +0530

    Delete server directory

--- server/models/Setting.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SettingSchema = new mongoose.Schema({
-  depositWhatsapp: { type: String, default: "" },
-  withdrawWhatsapp: { type: String, default: "" },
-});
-
-module.exports = mongoose.model('Setting', SettingSchema);
--- server/routes/signup.js
+++
@@ -1,9 +0,0 @@
-// server/routes/signup.js
-const express = require('express');
-const router = express.Router();
-const { register } = require('../controllers/authController');
-
-// POST /api/auth/signup
-router.post('/signup', register);
-
-module.exports = router;
