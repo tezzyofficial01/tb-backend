58964e4090c2ed332fda9238bd02c4cafff9661c
tezzyofficial01 <tezzyofficial01@gmail.com>
2025-07-26 15:29:21 +0530

    Update roundEngine.js

--- roundEngine.js
+++ roundEngine.js
@@ -1,33 +1,26 @@
-// roundEngine.js
 const axios = require('axios');
-
 let round = 1;
 
 function startGameEngine() {
   setInterval(async () => {
-    try {
-      console.log(`⏱ ROUND ${round} started`);
+    console.log(`⏱ ROUND ${round} started`);
 
-      // 10s - lock winner
-      setTimeout(() => {
-        axios.post('http://localhost:5000/bets/lock-winner', { round });
-      }, 10000);
+    // 10s
+    setTimeout(() => {
+      axios.post('https://tb-backend-tnab.onrender.com/api/bets/lock-winner', { round });
+    }, 10000);
 
-      // 35s - announce winner
-      setTimeout(() => {
-        axios.post('http://localhost:5000/bets/announce-winner', { round });
-      }, 35000);
+    // 35s
+    setTimeout(() => {
+      axios.post('https://tb-backend-tnab.onrender.com/api/bets/announce-winner', { round });
+    }, 35000);
 
-      // 40s - payout + move to next round
-      setTimeout(() => {
-        axios.post('http://localhost:5000/bets/distribute-payouts', { round });
-        round += 1;
-      }, 40000);
-
-    } catch (err) {
-      console.error('⛔ Engine error:', err.message);
-    }
-  }, 40000); // new round every 40 sec
+    // 40s
+    setTimeout(() => {
+      axios.post('https://tb-backend-tnab.onrender.com/api/bets/distribute-payouts', { round });
+      round += 1;
+    }, 40000);
+  }, 40000);
 }
 
 module.exports = startGameEngine;
