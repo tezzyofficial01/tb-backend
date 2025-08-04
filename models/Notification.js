f90000e3361591aef0e5a97501e1b07892c1aa53
tezzyofficial01 <tezzyofficial01@gmail.com>
2025-08-04 11:13:54 +0530

    Create Notification.js

---
+++ models/Notification.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  type: { type: String, enum: ['deposit', 'withdraw'], required: true },
+  amount: Number,
+  message: String,
+  read: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
